d67bd8763b1722a741699a28701d932b077c4357
zhongzx8080 <zhongzx8080@qq.com>
2020-03-21 20:03:14 +0800

    base64编码

--- src/app/routes/base64/base64-encode.component.ts
+++ src/app/routes/base64/base64-encode.component.ts
@@ -1,14 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Base64 } from "js-base64";
 
 @Component({
-  selector: 'app-base64-encode',
-  templateUrl: './base64-encode.component.html',
+  selector: "app-base64-encode",
+  templateUrl: "./base64-encode.component.html",
+  styles: [
+    `
+      .clearBtn {
+        margin-bottom: 10px;
+      }
+    `
+  ]
 })
 export class Base64EncodeComponent implements OnInit {
+  rows = 15;
 
-  constructor() { }
+  originText = "";
+  encodedText = "";
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  textChange(evt) {
+    this.encodedText = this.base64Encode(this.originText);
   }
 
+  clearText() {
+    this.originText = "";
+    this.encodedText = "";
+  }
+
+  private base64Encode(str) {
+    let encodedStr = Base64.encode(str) || "";
+    return encodedStr;
+  }
 }
