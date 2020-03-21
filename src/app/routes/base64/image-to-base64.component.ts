63ec4fa9a3373f6f83822711f1673cb5cb0671c7
zhongzx8080 <zhongzx8080@qq.com>
2020-03-21 18:38:58 +0800

    图片转Base64

--- src/app/routes/base64/image-to-base64.component.ts
+++ src/app/routes/base64/image-to-base64.component.ts
@@ -1,14 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UploadFile, NzMessageService } from "ng-zorro-antd";
 
 @Component({
-  selector: 'app-image-to-base64',
-  templateUrl: './image-to-base64.component.html',
+  selector: "app-image-to-base64",
+  templateUrl: "./image-to-base64.component.html",
+  styles: [
+    `
+    .clipArea {
+      opacity: 0;
+    }
+    `
+  ]
 })
 export class ImageToBase64Component implements OnInit {
+  fileList: UploadFile[] = [];
 
-  constructor() { }
+  base64Image = "";
+
+  data: any[] = [];
+
+  constructor(private msg: NzMessageService) {}
 
   ngOnInit(): void {
+    this.loadData(1);
   }
 
+  beforeUpload = (file: UploadFile): boolean => {
+    this.fileList = [file];
+    let allowedTypes = [
+      "image/jpeg",
+      "image/png",
+      "image/jpeg",
+      "image/gif",
+      "image/bmp"
+    ];
+    const isJPG = allowedTypes.some(item => item === file.type);
+    if (!isJPG) {
+      this.msg.error("请选择图片类型文件");
+      return false;
+    }
+
+    this.previewFile(file);
+
+    return false;
+  };
+
+  previewFile(file) {
+    let reader = new FileReader();
+    let that = this;
+    reader.addEventListener(
+      "load",
+      () => {
+        that.base64Image = reader.result.toString();
+      },
+      false
+    );
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  }
+
+  loadData(pi: number): void {
+    this.data = new Array(pi).fill({}).map((_, index) => {
+      return {};
+    });
+  }
 }
